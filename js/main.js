ccbf3f3da7413821726209d84beb4a199b8e2149
takizawa-0203 <sona.usamaru1306@gmail.com>
2026-07-02 22:26:38 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 // ハンバーガーメニュー
-$(".openbtn").click(function () {
-    $(this).toggleClass('active');
+$(function(){
+
+    $(".openbtn").click(function(){
+        $(this).toggleClass("active");
+        $("#js_nav").toggleClass("panelactive");
+    });
+
+    $("#g-navi a").click(function(){
+        $(".openbtn").removeClass("active");
+        $("#js_nav").removeClass("panelactive");
+    });
+
 });
